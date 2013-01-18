725698501e6c3dc059183d103f415dec93e3217b
FotoVerite <matt@novafabrica.com>
2013-01-18 16:38:37 -0500

    [Gem, Config] updated config for correct generators ectra.

--- Gemfile
+++ Gemfile
@@ -8,8 +8,8 @@ gem 'bcrypt-ruby', '~> 3.0.0'
 gem 'jquery-rails'
 gem 'turbolinks'
 gem 'jbuilder', '~> 1.0.1'
-
-
+gem 'sunspot'
+gem 'sidekiq'
 # Gems used only for assets and not required
 # in production environments by default.
 group :assets do
--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,36 @@ module Nftixs
 
     # Custom directories with classes and modules you want to be autoloadable.
     # config.autoload_paths += %W(#{config.root}/extras)
+    config.autoload_paths += %W(
+      #{config.root}/app/models/concerns
+      #{config.root}/lib
+      #{config.root}/lib/nova_fabrica
+    )
+
+    config.encoding = "utf-8"
+
+    # Configure sensitive parameters which will be filtered from the log file.
+    config.filter_parameters += [:password]
+
+    require "#{config.root}/lib/sunspot_addedums"
+
+    config.generators.stylesheet_engine = :sass
+    config.assets.enabled = true
+    config.generators do |g|
+      g.test_framework :rspec, :views => false, :fixture => true
+      g.fixture_replacement :factory_girl, :dir => 'spec/factories'
+    end
+    config.assets.precompile += [
+      'public.css', 'staff.css',
+      'staff.js', 'public.js'
+    ]
+    config.generators do |g|
+      g.orm             :active_record
+      g.template_engine :erb
+      g.test_framework :rspec, :fixture => true, :views => false
+      g.fixture_replacement :factory_girl, :dir => "spec/factories"
+      g.stylesheets     false
+      g.javascripts     false
+    end
   end
 end
