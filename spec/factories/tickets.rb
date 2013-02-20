d241ef10c357d32902f3905580fc71e8fad890cc
FotoVerite <mzbphoto@gmail.com>
2013-02-20 15:32:04 +0700

    [rspec] Fixes tickets_controller spec

--- spec/factories/tickets.rb
+++ spec/factories/tickets.rb
@@ -4,7 +4,7 @@ FactoryGirl.define do
     name Faker::Company.bs
     description Faker::Lorem.paragraph(3)
     status ['open', 'closed', 'pending'].shuffle[0..(0)].first
-    ticket_group {|a| a.association(:ticket_group) }
+    ticket_group_id { FactoryGirl.create(:ticket_group).id }
   end
 
 
